e5e6066df2353428cf1dffc5909fde9536691a20
alherca <alherca.co@gmail.com>
2014-10-19 21:55:55 -0500

    Create primer.sh

---
+++ usr/local/opendomo/daemons/primer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          odauto
+# Required-Start:    
+# Required-Stop:
+# Should-Start:      
+# Default-Start:     1 2 3 4 5
+# Default-Stop:      0 6
+# Short-Description: Update weather station
+# Description:       Update weather station
+#
+### END INIT INFO
+### Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
