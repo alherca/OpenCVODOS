81e9ff6043e3607cdd6984386852e6f105294a7b
alherca <alherca.co@gmail.com>
2014-10-30 11:19:56 -0500

    Update configureOpenCVODOS.sh

--- usr/local/opendomo/services/config/configureOpenCVODOS.sh
+++ usr/local/opendomo/services/config/configureOpenCVODOS.sh
@@ -6,3 +6,9 @@
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
 DEVNAME="opencvodos"
+CFGFILE="/etc/opendomo/$DEVNAME.conf"
+
+if test -f $CFGFILE;
+then
+	source $CFGFILE
+fi
