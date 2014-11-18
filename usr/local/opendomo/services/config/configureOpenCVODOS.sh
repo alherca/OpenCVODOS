cfa4ecbe5ff70ab91bb9e730c553701d54b6a529
alherca <alherca.co@gmail.com>
2014-11-17 23:16:13 -0500

    Update configureOpenCVODOS.sh
    Define DIRs

--- usr/local/opendomo/services/config/configureOpenCVODOS.sh
+++ usr/local/opendomo/services/config/configureOpenCVODOS.sh
@@ -6,7 +6,11 @@
 # Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
 
 DEVNAME="opencvodos"
+CONFIGDIR="/etc/opendomo/vision/filters/"
+CAMDIR="/etc/opendomo/control/"
 CFGFILE="/etc/opendomo/$DEVNAME.conf"
+
+test -d $CONFIGDIR || mkdir $CONFIGDIR
 
 if test -f $CFGFILE;
 then
