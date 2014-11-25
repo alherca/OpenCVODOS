f05beeb752d7130bb103e18f12d6e93aa8de2c0b
alherca <alherca.co@gmail.com>
2014-11-24 23:38:52 -0500

    Create opencvodos.sh

---
+++ usr/local/opendomo/daemons/opencvodos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          opencvodos
+# Required-Start:    
+# Required-Stop:
+# Should-Start:      
+# Default-Start:     1 2 3 4 5
+# Default-Stop:      0 6
+# Short-Description: Vision
+# Description:       Vision
+#
+### END INIT INFO
+
+# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
