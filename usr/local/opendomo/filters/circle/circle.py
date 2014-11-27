173869a6cd49bf0124472eff11d0985f57873470
alherca <alherca.co@gmail.com>
2014-11-27 00:23:17 -0500

    Create circle.py

---
+++ usr/local/opendomo/filters/circle/circle.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+# Load the images
+image =cv2.imread('images.jpg')
+
+output = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# detect circles in the image
+circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
+ 
+# ensure at least some circles were found
+if circles is not None:
+	# convert the (x, y) coordinates and radius of the circles to integers
+	circles = np.round(circles[0, :]).astype("int")
+ 
+	# loop over the (x, y) coordinates and radius of the circles
+	for (x, y, r) in circles:
+		# draw the circle in the output image, then draw a rectangle
+		# corresponding to the center of the circle
+		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+cv2.imwrite('facesale.png',output)
+ 
+	# show the output image
+#	cv2.imshow("output", np.hstack([image, output]))
+#	cv2.waitKey(0)
