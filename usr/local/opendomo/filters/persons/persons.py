213bd9234becc6bf0b7d385171c0e02afe157c53
alherca <alherca.co@gmail.com>
2014-12-10 22:59:12 -0500

    Update persons.py

--- usr/local/opendomo/filters/persons/persons.py
+++ usr/local/opendomo/filters/persons/persons.py
@@ -1,5 +1,5 @@
 #!/bin/python
-#desc:Filter persons detection using a image
+#desc: Filter detection of persons using an image
 #package:opencvodos
 
 ### Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
