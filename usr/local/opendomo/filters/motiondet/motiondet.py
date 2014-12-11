9761b7ba05e74a2698b7823635f80bb5004c6d95
alherca <alherca.co@gmail.com>
2014-12-10 22:39:56 -0500

    Update motiondet.py

--- usr/local/opendomo/filters/motiondet/motiondet.py
+++ usr/local/opendomo/filters/motiondet/motiondet.py
@@ -1,5 +1,5 @@
 #!/bin/python
-#desc:Filter motion detection using a webcam
+#desc:Filter motion detection using a image
 #package:opencvodos
 
 ### Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
