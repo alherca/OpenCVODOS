c53eea24d9f74a9b1f1c8b5c7d7cff97006c5ed2
alherca <alherca.co@gmail.com>
2014-12-16 16:11:51 -0500

    Create configureOpenCVODOS.sh

---
+++ usr/local/opendomo/bin/configureOpenCVODOS.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#desc:Configure OpenCVODOS
+#package:opencvodos
+#type:local
+
+# Copyright(c) 2014 OpenDomo Services SL. Licensed under GPL v3 or later
+
+DEVNAME="opencvodos"
+CONFIGDIR="/etc/opendomo/vision/filters"
+CAMDIR="/etc/opendomo/control/"
+
+test -d $CONFIGDIR || mkdir $CONFIGDIR
+
+
+if ! test -z "$2"
+then
+	if test -f $CAMDIR/$1.conf
+	then
+		ID="$1"
+		NAME="$2"
+		test -d $CONFIGDIR/$ID || mkdir $CONFIGDIR/$ID
+		FILENAME="$CONFIGDIR/$ID.conf"
+		echo "ID=$ID" > $FILENAME
+		echo "NAME='$NAME'" >> $FILENAME
+	else
+		echo "#INFO No cameras were found, with ID:$1"
+	fi
+else
+	cd $CAMDIR
+	for i in *.conf; 
+	do
+	if test "$i" = "*.conf"
+	then
+		echo "#INFO No cameras were found"
+		echo "actions:"
+		echo "	addControlDevice.sh "
+		echo
+		exit 0
+	fi
+	source ./$i
+	ID=`basename $i | cut -f1 -d.`
+	echo "-ID:$ID	Name: $NAME"
+	done
+	echo "Usage: $0 [ID] [Name]"
+fi
