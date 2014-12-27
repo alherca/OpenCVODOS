048485cafbfd3dbf9ac4a2842e3e0923dbb132a7
alherca <alherca.co@gmail.com>
2014-12-26 23:24:00 -0500

    Update circle.py

--- usr/local/opendomo/filters/circle/circle.py
+++ usr/local/opendomo/filters/circle/circle.py
@@ -18,36 +18,34 @@ config = ConfigParser.ConfigParser()
 config.read('/etc/opendomo/vision/' + ID + '/filters/circle.conf')
 confID = config.get('Definition', 'ID')
 confNAME = config.get('Definition', 'NAME')
-print confID
-print confNAME
 
+#Only developer
+#print confID
+#print confNAME
+
+# Load images
 image = cv2.imread('/var/www/data/' + confID + '.jpg')
 
-# Load the images
+#Only developer
 #image =cv2.imread('images.jpg')
 
 output = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # detect circles in the image
 circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
- 
-# ensure at least some circles were found
+
 if circles is not None:
 	# convert the (x, y) coordinates and radius of the circles to integers
 	circles = np.round(circles[0, :]).astype("int")
 	# loop over the (x, y) coordinates and radius of the circles
 	for (x, y, r) in circles:
-		# draw the circle in the output image, then draw a rectangle
-		# corresponding to the center of the circle
+		# draw the circle and a rectangle in the output image
+		# center of the circle
 		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
 		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 	
-	# save the output image
+	# save log
 subprocess.call(["/bin/logevent", "circle", "opencvodos", "Circle process in " + confID +  " /var/www/data/" + confID + "_circle.png"])
-	# logevent motiondet opencvodos "Motion detected in " + confID + " /var/www/data/" + confID + "_circle.png"
 
+	# save the output image
 cv2.imwrite('/var/www/data/' + confID + '_circle.png',output)
- 
-	# show the output image only PC
-#	cv2.imshow("output", np.hstack([image, output]))
-#	cv2.waitKey(0)
