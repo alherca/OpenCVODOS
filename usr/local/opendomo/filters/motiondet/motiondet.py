b783588c5d4233fd3f4a2745b4168f5572a7b64d
alherca <alherca.co@gmail.com>
2014-12-26 23:27:09 -0500

    Update motiondet.py

--- usr/local/opendomo/filters/motiondet/motiondet.py
+++ usr/local/opendomo/filters/motiondet/motiondet.py
@@ -18,9 +18,12 @@ config = ConfigParser.ConfigParser()
 config.read('/etc/opendomo/vision/' + IDC + '/filters/motiondet.conf')
 confID = config.get('Definition', 'ID')
 confNAME = config.get('Definition', 'NAME')
-print confID
-print confNAME
 
+#Only developer
+#print confID
+#print confNAME
+
+# Load images
 imga = cv2.imread('/var/www/data/' + confID + '.jpg')
 imgb = cv2.imread('/var/www/data/prev_' + confID + '.jpg')
 
@@ -30,21 +33,30 @@ imgbc = cv2.cvtColor(imgb, cv2.COLOR_BGR2GRAY)
 img1 = imgac[10:330, 10:870]
 img2 = imgbc[10:320, 10:870]
 
+#Only developer
 # cv2.imwrite('/var/www/data/' + confID + '_motiondet.png',imgb)
 
 start = time.clock()
+#diff images, detect motion
 d = cv2.absdiff(img1,img2)
 s = d.sum()
 t = time.clock() - start
-print 'absdiff ', t
-print s
+
+#Only developer
+#print 'absdiff ', t
+#print s
 
 if s != 0 :
+  # save log
   subprocess.call(["/bin/logevent", "motiondet", "opencvodos", "Motion detected in " + confID +  " /var/www/data/" + confID + "_motiondet.png"])
+  # save output image
   cv2.imwrite('/var/www/data/' + confID + '_motiondet.png',imgb)
 
+#Only developer
 start = time.clock()
 s = cv2.norm(img1, img2, cv2.NORM_L1)
 t = time.clock() - start
-print 'norm L1 ',  t
-print s
+
+#Only developer
+#print 'norm L1 ',  t
+#print s
